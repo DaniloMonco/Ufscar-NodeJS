89d88c58b308feb38e16be1f3fbbe22d273d006f
DaniloMonco <40800408+DaniloMonco@users.noreply.github.com>
2019-02-14 23:53:45 -0200

    Add files via upload

---
+++ Script/SQLServerDatabase_Tables.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE [Ufscar2]
+GO
+
+USE [Ufscar2]
+GO
+
+
+CREATE TABLE [dbo].[USER](
+	[UserId] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	[Login] [varchar](25) NOT NULL,
+	[Name] [varchar](60) NOT NULL,
+	[Email] [varchar](100) NOT NULL,
+	[Password] [varchar](20) NOT NULL
+	)
+GO
+
+
+CREATE TABLE [dbo].[USERMOVIES](
+	[UserId] [int] NOT NULL CONSTRAINT [fk_user] FOREIGN KEY([UserId]) REFERENCES [dbo].[USER] ([UserId]),
+	[MovieId] [int] NOT NULL ,
+PRIMARY KEY
+(
+	[UserId],
+	[MovieId]
+))
+
+
+
